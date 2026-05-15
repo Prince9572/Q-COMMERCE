9ef5e1bbbdf028d855475d382cfe06a5f8bf77b6
Prince9572 <princeraj854201@gmail.com>
2026-05-15 21:37:41 +0530

    footer added and designing start of CartSidebar

---
+++ src/context/CartContext.tsx
@@ -0,0 +1,27 @@
+import { createContext, type ReactNode } from "react";
+import type { CartItem, Product } from "../types";
+
+interface CartContextType {
+    items: CartItem[];
+    addToCart: (product: Product, quantity)=> void;
+    removeFromCart: (productId: string) => void;
+    updateQuantity: (productId: string, quantity: number) => void;
+    clearCart: () => void;
+    cartCount: number;
+    cartTotal: number;
+    isCartopen: boolean;
+    setIsCartOpen: (open: boolean) => void;
+
+}
+
+const CartContext = createContext<CartContextType | undefined >(undefined)
+
+export function CartProvider({children}: {children : ReactNode}){
+    return <CartContext.Provider value={{}}>
+        {children}
+    </CartContext.Provider>
+}
+
+export function useCart(){
+    
+}
